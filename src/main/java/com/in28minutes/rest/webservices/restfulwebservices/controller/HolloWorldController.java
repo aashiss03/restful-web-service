fbb50e9ab98fa8bec7ad026f39eca2671b72081a
aashiss03@gmail.com <aashiss03@gmail.com>
2021-11-18 14:19:15 -0500

    added path variable

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controller/HolloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controller/HolloWorldController.java
@@ -1,7 +1,7 @@
 package com.in28minutes.rest.webservices.restfulwebservices.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.rest.webservices.restfulwebservices.model.HelloWorldBean;
@@ -18,6 +18,13 @@ public class HolloWorldController {
 	public HelloWorldBean helloWorldBean() {
 
 	return new HelloWorldBean("Hello-World-Bean");
+	
+	}
+	@GetMapping("/Hello-World-Bean/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+
+	return new HelloWorldBean(String.format("Hello World, %s",name));
+	
 	}
 
 }
