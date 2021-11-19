8a969753f5610b852c51bbe3bbb43dc1bf5dc8da
aashiss03@gmail.com <aashiss03@gmail.com>
2021-11-19 17:32:01 -0500

    user not found exception handing added

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomExceptionHandler.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomExceptionHandler.java
@@ -1,11 +1,33 @@
 package com.in28minutes.rest.webservices.restfulwebservices.exception;
 
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestController
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	@ExceptionHandler(Exception.class)
+	 ResponseEntity<Object> handleAllException(Exception ex, WebRequest request){
+		 
+		ExceptionResponse exceptionResp= new ExceptionResponse(new Date(),	 ex.getMessage(),request.getDescription(false));
+		
+		return new ResponseEntity<Object>(exceptionResp, HttpStatus.INTERNAL_SERVER_ERROR);
+	 }
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	 ResponseEntity<Object> handleUserNotException(Exception ex, WebRequest request){
+		 
+		ExceptionResponse exceptionResp= new ExceptionResponse(new Date(),	 ex.getMessage(),request.getDescription(false));
+		
+		return new ResponseEntity<Object>(exceptionResp, HttpStatus.NOT_FOUND);
+	 }
 
 }
